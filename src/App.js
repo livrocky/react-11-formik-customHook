ea2b64c0ca757f81e1a724eefa0335c9749b3eb4
rokas <rokasra@gmail.com>
2022-06-21 16:15:47 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import { useState } from 'react';
+import { useContext } from 'react';
 import { Route, Switch } from 'react-router';
+import { Redirect } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import AboutPage from './pages/About/AboutPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
-import AuthProvider from './store/AuthContext';
+import AuthProvider, { AuthContext } from './store/AuthContext';
 
 function App() {
+  const { isUserLoggedIn } = useContext(AuthContext);
+  console.log('isUserLoggedIn===', isUserLoggedIn);
   return (
     <AuthProvider>
       <div className='App'>
         <Header />
         <Switch>
-          {/* negeneruoti route /about jei nesam prisilogine */}
-          <Route path='/about'>
-            <AboutPage />
-          </Route>
+          <Route path='/about'>{!isUserLoggedIn ? <AboutPage /> : <Redirect to='/login'></Redirect>}</Route>
           <Route exact path='/login'>
             <LoginPage />
           </Route>
